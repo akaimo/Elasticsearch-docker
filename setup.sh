b48c12c8e3f16caf109020b69222303d454415db
shuhei <kawashu0723@gmail.com>
2016-05-29 15:27:57 +0900

    add setup explanation

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+# 初期設定
+# テスト用のindexをPOST、テストデータをPUTする
+# これでKibanaからデータの確認ができるようになる
 curl -X POST $(docker-machine ip akaimo):9200/ldgourmet -d @data/mapping.json && \
   curl -X PUT $(docker-machine ip akaimo):9200/ldgourmet/restaurant/1 -d @data/restaurant_test.json && \
   curl -X PUT $(docker-machine ip akaimo):9200/ldgourmet/rating/1 -d @data/rating_test.json
